c850f86e4accf30b260662a0678cda6529980c76
Shivani verma <shivani.vr.verma@gmail.com>
2023-10-15 22:10:22 +0530

    done

--- tempratureConverter/script.js
+++ tempratureConverter/script.js
@@ -7,19 +7,19 @@ function TempratureCalc() {
 
 
     function celsiTofarh(celsi){
-        let fah = parseFloat((celsi*9/5)+32)
+        let fah = parseFloat((celsi*9/5)+32).toFixed(2)
         // console.log(`${fah} farh`);
         // document.querySelector('cl').innerHTML = `${ans}°C`
         return fah
     }
     function farhTcels(farh){
-        let cls = parseFloat((farh-32)*5/9)
+        let cls = parseFloat((farh-32)*5/9).toFixed(2)
         // console.log(`${cls} cels`);
         return cls
     }
 
     function kaltocel(kal){
-        let kl = parseFloat(kal - 273.15)
+        let kl = parseFloat(kal - 273.15).toFixed(2)
         // console.log(`${cls} cels`);
         return kl
     }
@@ -30,13 +30,18 @@ function TempratureCalc() {
     if (valOfselct == 'cel') {
         ans = celsiTofarh(inputTem)
         document.querySelector('#n').innerHTML = `${ans}°F`
+        document.querySelector('p').innerHTML = `Celsius to Fahrenheit`
     } else if( valOfselct == 'Fah') {
         ans = farhTcels(inputTem)
         document.querySelector('#n').innerHTML = `${ans}°c`
+        document.querySelector('p').innerHTML = ` Fahrenheit to Celsius`
+
         // console.log();
     }else{
         ans = kaltocel(inputTem)
         document.querySelector('#n').innerHTML = `${ans}°c`
+        document.querySelector('p').innerHTML = ` Kalvin to Celsius`
+
     }
 
     
